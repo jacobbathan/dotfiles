64f29b751304c0421c541100e61e33adde2f5f9c
jacob <jacobbathan@gmail.com>
2021-12-06 18:28:11 -0800

    added qtile/kitty configs, modified install.sh

--- install.sh
+++ install.sh
@@ -24,17 +24,26 @@ ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 rm -rf "$XDG_CONFIG_HOME/i3"
 ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
+# qtile
+rm -rf "$XDG_CONFIG_HOME/qtile"
+ln -s "$DOTFILES/qtile" "$XDG_CONFIG_HOME"
+
 # zsh
 mkdir -p "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
 ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 
+
 # zsh auto complete
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
-# fontz
+# kitty terminal
+rm -rf "$XDG_CONFIG_HOME/kitty"
+ln -sf "$XDG_CONFIG_HOME/kitty" "$XDG_CONFIG_HOME"
+
+# fonts
 mkdir -p "$XDG_DATA_HOME"
 cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
