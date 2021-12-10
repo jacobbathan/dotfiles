9798c9f93c803079181dee89fad36e2166d48107
jacob <jacobbathan@gmail.com>
2021-12-09 19:39:23 -0800

    added autostart for nitrogen wallpapers in qtile

---
+++ qtile/autostart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nitrogen --restore --set-zoom-fill --random ~/Projects/nordic-wallpapers/wallpapers
--- qtile/config.py
+++ qtile/config.py
@@ -26,10 +26,12 @@
 
 from typing import List  # noqa: F401
 
-from libqtile import bar, layout, widget
+from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+import os
+import subprocess
 
 mod = "mod4"
 terminal = guess_terminal()
@@ -197,12 +199,7 @@ reconfigure_screens = True
 # focus, should we respect this or not?
 auto_minimize = True
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-wmname = "LG3D"
+@hook.subscribe.startup_once
+def start_once():
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    subprocess.call([home])
